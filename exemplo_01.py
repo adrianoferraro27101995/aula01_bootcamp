d4d56057cfe53917f424adbd169bbbf46a186a04
adrianoferraro27101995 <adriano.ferraro27101995@gmail.com>
2024-04-16 20:44:11 -0300

    exemplo de utilização da função print

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,1 +1,13 @@
 print("Hello world!")
+
+print("Alguma coisa")
+
+print(3)
+
+print(3 + 4)
+
+print("Olá" + " " + "Turma")
+
+input("Digite seu nome: ")
+
+print("Olá, " + input("Digite seu nome: ") + "!")
