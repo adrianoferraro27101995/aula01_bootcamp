871716d971d84942ff9f45a35547373f48d2f54c
adrianoferraro27101995 <adriano.ferraro27101995@gmail.com>
2024-04-16 20:45:57 -0300

    programa que captura valores e soma, e define variaveis do Python

---
+++ exemplo_03.py
@@ -0,0 +1,19 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+primeiro_valor = int(input("Digite um valor:"))
+
+segundo_valor = int(input("Digite outro valor:"))
+
+print(primeiro_valor + segundo_valor)
+
+numero = 1 # int
+numero_decimal = 4.78 # float
+nome = "Adriano" # string
+verdadeiro = True # bool
+print(type(numero))
+
+AWS_SENHA = 123
+
+_AWS_SENHA = 123
+
+
